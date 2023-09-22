9414d3a3b71555182d00f67be2863353190f91d9
apikmeister <afiq.mohamad90@gmail.com>
2023-09-22 15:19:38 +0800

    mb didnt mean to uncomment

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
       <h1 className="mb-8 text-center text-2xl font-black">
         Coming really soon..
       </h1>
-      {posts.map((post, idx) => (
+      {/* {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
-      ))}
+      ))} */}
     </div>
   );
 }
