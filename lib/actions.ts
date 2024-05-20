e9ecac5aff75682b6cedf50bcb53320dc62023f4
apikmeister <afiq.mohamad90@gmail.com>
2024-05-20 14:18:12 +0800

    fix views and change config

--- lib/actions.ts
+++ lib/actions.ts
@@ -6,21 +6,18 @@ import { type Session } from 'next-auth';
 import { queryBuilder } from 'lib/database';
 import { revalidatePath, unstable_noStore as noStore } from 'next/cache';
 import { authOptions } from './auth';
+import { sql } from 'kysely';
 
 export async function increment(slug: string) {
   noStore();
-  const data = await queryBuilder
-    .selectFrom('views')
-    .where('slug', '=', slug)
-    .select(['count'])
-    .execute();
-
-  const views = !data.length ? 0 : Number(data[0].count);
-
-  return queryBuilder
+  
+  await queryBuilder
     .insertInto('views')
     .values({ slug, count: 1 })
-    .onDuplicateKeyUpdate({ count: views + 1 })
+    .onConflict((oc) => oc
+      .column('slug')
+      .doUpdateSet({ count: sql`${sql.ref('views.count')} + 1` })
+    )
     .execute();
 }
 
